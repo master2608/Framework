587f667bb0f7cf5591d920cec7c905df746bfb29
master2608 <arthurmaster46@gmail.com>
2026-08-05 11:05:18 -0300

    Adiciona arquivos do app

--- clima/lib/main.dart
+++ clima/lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 import 'pages/tela_clima.dart';
 
 void main() {
-  runApp(const ClimaAgoraApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const ClimaAgoraApp(),
+    ),
+  );
 }
 
 class ClimaAgoraApp extends StatelessWidget {
@@ -13,6 +20,12 @@ class ClimaAgoraApp extends StatelessWidget {
     return MaterialApp(
       title: 'Clima Agora',
       debugShowCheckedModeBanner: false,
+      
+      
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
